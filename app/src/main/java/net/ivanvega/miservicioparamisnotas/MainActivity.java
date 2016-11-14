4a4a2fcab17f9298edb35faff44b802e8cde3bd4
iscgivox <isc.givox@gmail.com>
2016-11-14 08:10:38 -0600

    SE CODIFICO EL SERVICIO Y SE MANDO LLAMAR

--- app/src/main/java/net/ivanvega/miservicioparamisnotas/MainActivity.java
+++ app/src/main/java/net/ivanvega/miservicioparamisnotas/MainActivity.java
@@ -1,13 +1,28 @@
 package net.ivanvega.miservicioparamisnotas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnStart_click(View v) {
+        startService(
+                new Intent(this, MySuperServiceNotas.class)
+        );
+    }
+
+    public void btnStopService_click(View v) {
+        stopService(
+                new Intent(this, MySuperServiceNotas.class)
+        );
     }
 }
--- app/src/main/java/net/ivanvega/miservicioparamisnotas/MySuperServiceNotas.java
+++ app/src/main/java/net/ivanvega/miservicioparamisnotas/MySuperServiceNotas.java
@@ -2,8 +2,11 @@ package net.ivanvega.miservicioparamisnotas;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by SERVIDOR on 11/11/2016.
@@ -11,11 +14,54 @@ import android.support.annotation.Nullable;
 
 
 public class MySuperServiceNotas extends Service {
+
+
+    private Handler handler =
+            new Handler();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    @Override
+    public void onCreate() {
 
+        super.onCreate();
+
+        Log.d("MYSIMPLESERVICE","SERVICIO ONCREATE");
+        Toast.makeText(this, "SERVICIO ONCREATE", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+
+        Log.d("MYSIMPLESERVICE","SERVICIO NICIADO");
+        Toast.makeText(this, "SERVICIO INICIADO", Toast.LENGTH_LONG).show();
+
+        handler.postDelayed(runnable, 5000);
+
+        return START_NOT_STICKY;
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+            Log.d("HILOCORRIENdo", "HILOCORRIENdo");
+            handler.postDelayed(this,2000);
+        }
+
+    };
+
+    @Override
+    public void onDestroy() {
+        Log.d("MYSIMPLESERVICE","SERVICIO PARADO");
+        Toast.makeText(this, "SERVICIO PARADO", Toast.LENGTH_LONG).show();
+
+        super.onDestroy();
+    }
 }
